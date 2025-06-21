1b8cd7a20472cbb99c82e52917293569fec866d5
FrootNoot <froot3noot@gmail.com>
2025-06-22 11:36:29 +1200

    button 3d effect and css

--- frontend/src/components/Home/Description.js
+++ frontend/src/components/Home/Description.js
@@ -1,33 +1,28 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from "react";
 import styles from "./Description.module.css";
-import { Dumbbell, Goal, NotebookTabs  } from 'lucide-react';
+import { Dumbbell, Goal, NotebookTabs } from 'lucide-react';
 
 function Description() {
-
   return (
     <div className={styles.descriptionContainer}> 
-
-
-      <div className={styles.descriptionItem}> 
-        <Dumbbell className={styles.icon}  color="black" />
+      <Link to="/workouts" className={styles.descriptionItem}>
+        <Dumbbell className={styles.icon}/>
         <h1>View My Workouts</h1>
-        <h3>See what weights, sets and reps I do for my workouts.</h3>
-      </div>
+        <h3>See what weights, sets, and reps I do for my workouts.</h3>
+      </Link>
 
-
-      <div className={styles.descriptionItem}> 
-        <Goal className={styles.icon}  color="black" />
+      <Link to="/goals" className={styles.descriptionItem}>
+        <Goal className={styles.icon} />
         <h1>Track My Goals</h1>
-        <h3>Check my progress towards my goals and see how they are coming along !</h3>
-      </div>
-      
-      <div className={styles.descriptionItem}> 
-        <NotebookTabs  className={styles.icon}  color="black" />
+        <h3>Check my progress towards my goals and see how they are coming along!</h3>
+      </Link>
+
+      <Link to="/log" className={styles.descriptionItem}>
+        <NotebookTabs className={styles.icon} />
         <h1>My Workout Log</h1>
         <h3>A logbook for myself to record everything I need to stay consistent.</h3>
-
-      </div>
+      </Link>
     </div>
   );
 }
